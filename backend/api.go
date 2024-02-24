56b1cfd0a89b23bb9de0c89176704c073d282639
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2024-02-24 15:20:49 -0600

    idk

--- backend/api.go
+++ backend/api.go
@@ -284,6 +284,7 @@ func main() {
 	router := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowCredentials = true
+	config.AllowOrigins = []string{"localhost", "heron.cs.umanitoba.ca"}
 	router.Use(cors.New(config))
 
 	router.POST("/login/new", createAccount)
