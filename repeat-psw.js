f93f4b66a59bf3f5267760e241a73561c90f4ed3
niraaz1912 <niraaj.ojha@gmail.com>
2024-02-24 10:36:49 -0600

    password check

--- repeat-psw.js
+++ repeat-psw.js
@@ -1,13 +1,19 @@
-var password = document.getElementById("password")
-  , confirm_password = document.getElementById("confirm_password");
-
 function validatePassword(){
+  const password = document.getElementById('password');
+  const confirm_password = document.getElementById('repeat-psw');
   if(password.value != confirm_password.value) {
     confirm_password.setCustomValidity("Passwords Don't Match");
   } else {
-    confirm_password.setCustomValidity('');
+    confirm_password.setCustomValidity("");
   }
+
+  confirm_password.reportValidity();
 }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+window.addEventListener('DOMContentLoaded', () => {
+  const password = document.getElementById('password');
+  const confirm_password = document.getElementById('repeat-psw');
+
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword;
+})
